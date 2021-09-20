0328e9ae4672455580d64d63ec57b5e2939d4be2
maneth <manethyethmin22@gmail.com>
2021-09-20 13:09:30 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { View, StyleSheet, Text, FlatList, Alert } from 'react-native'
+import { uuid } from 'uuidv4'
 
-export default function App() {
+import ListItem from './components/ListItem'
+import Header from './components/Header'
+import AddItem from './components/AddItem'
+
+const App = () => {
+  const [items, setItems] = useState([
+    { id: uuid(), text: 'Milk' },
+    { id: uuid(), text: 'Eggs' },
+    { id: uuid(), text: 'Bread' },
+    { id: uuid(), text: 'Cheese' },
+  ])
+
+  const deleteItem = (id) => {
+    setItems((prevItems) => {
+      return prevItems.filter((item) => item.id !== id)
+    })
+  }
+
+  const addItem = (text) => {
+    if (text == '') {
+      Alert.alert('Error', 'Please enter an item', { text: 'Ok' })
+    } else {
+      setItems((prevItems) => {
+        return [{ id: uuid(), text }, ...prevItems]
+      })
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <AddItem addItem={addItem} />
+      <FlatList
+        data={items}
+        renderItem={({ item }) => {
+          return <ListItem item={item} deleteItem={deleteItem} />
+        }}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+})
+
+export default App
---
+++ components/AddItem.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+const AddItem = ({ addItem }) => {
+  const [text, setText] = useState('')
+
+  const onChange = (textValue) => {
+    setText(textValue)
+  }
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Add item.."
+        style={styles.input}
+        value={text}
+        onChangeText={onChange}
+      />
+      <TouchableOpacity
+        style={styles.btn}
+        onPress={() => {
+          addItem(text)
+          setText('')
+        }}
+      >
+        <Text style={styles.btnText}>
+          <Icon name="plus" size={20} /> Add Item
+        </Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  input: {
+    height: 60,
+    padding: 8,
+    fontSize: 16,
+  },
+  btn: {
+    backgroundColor: '#c2bad8',
+    padding: 9,
+    margin: 5,
+  },
+  btnText: {
+    color: 'darkslateblue',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+})
+
+export default AddItem
